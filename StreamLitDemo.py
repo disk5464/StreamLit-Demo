d24b05dd6c3881527ea08616436c2dc2efe0c13d
Disk <scoobiedew@comcast.net>
2025-10-08 13:02:33 -0400

    added stuff

--- StreamLitDemo.py
+++ StreamLitDemo.py
@@ -1,6 +1,17 @@
 import streamlit as st
-
+import requests
+import uuid
+import json
+import urllib.parse
+import sys
+import os
+from colorama import Fore, Back, Style
+########################################################################
 st.write("Hello World")
 text = st.text_input("Type something:")
 if text:
     st.write("You wrote:", text)
+########################################################################
+radio_id_input = input("Enter Radio ID or VIN: ").upper()
+uuid4 = str(uuid.uuid4())
+print(uuid4)
